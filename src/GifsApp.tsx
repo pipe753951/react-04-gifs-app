54fa1603f9e246f076795d27b5d5fce1d55f667c
David Puentes <pipe01112009@gmail.com>
2026-03-13 08:08:18 -0500

    Finishing class #75 with homework.

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { sampleGifs } from "./sample-data/gifs.sample";
 
 import { AppHeader } from "./shared/components/AppHeader";
@@ -7,13 +9,35 @@ import { SearchBar } from "./shared/components/SearchBar";
 import { GifList } from "./gifs/components/GifList";
 
 export const GifsApp = function () {
-  // const [previousSearches, setPreviousSearches] = useState([["Libros"]]);
+  const [previousSearches, setPreviousSearches] = useState<string[]>([]);
 
   const handleTermClicked = (term: string) => {
     console.log({ term });
   };
   const handleSearch = (query: string) => {
-    console.log({ query });
+    // On a new variable, delete unnecesary spaces at from both ends of the received query.
+    let processedQuery = query.trim();
+
+    // If processedQuery is empty, return.
+    if (!query.trim()) return;
+
+    // Turn processedQuery into lower case.
+    processedQuery = processedQuery.toLowerCase();
+
+    // Create new searched terms list.
+    // Check if query has been previously is registered;
+    // if so, then remove registered query to "move it" to the first place.
+    const processedPreviousSearches = previousSearches.filter(
+      (term) => term !== processedQuery,
+    );
+
+    // If there are more than 7 terms, remove the last one (New query will be added on update state).
+    if (processedPreviousSearches.length > 7) {
+      processedPreviousSearches.pop();
+    }
+
+    // Now, set newPreviousSearches on previousSearches state.
+    setPreviousSearches([processedQuery, ...processedPreviousSearches]);
   };
 
   return (
@@ -35,7 +59,7 @@ export const GifsApp = function () {
       {/* Búsquedas previas */}
       <PreviousSearches
         onLabelClick={handleTermClicked}
-        previousSearchesList={["Libros", "Risas", "Naturaleza", "Química"]}
+        previousSearchesList={previousSearches}
       />
 
       {/* Gifs */}
