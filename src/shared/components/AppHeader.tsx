390963bbc2bc857e53bf857d9ee2f20fe816d860
David Puentes <pipe01112009@gmail.com>
2026-03-17 19:57:42 -0500

    Commenting parts of AppHeader.

--- src/shared/components/AppHeader.tsx
+++ src/shared/components/AppHeader.tsx
@@ -6,7 +6,10 @@ interface Props {
 export const AppHeader = function ({ title, description }: Props) {
   return (
     <div className="flex items-center flex-col">
+      {/* Heading */}
       <h1 className="mt-7 text-5xl font-bold">{title}</h1>
+
+      {/* Description */}
       {description && <p className="my-2">{description}</p>}
     </div>
   );
