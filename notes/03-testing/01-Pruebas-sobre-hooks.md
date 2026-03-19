303fc42d2577321ed4cb939009cc8fa3e4be6b7b
David Puentes <pipe01112009@gmail.com>
2026-03-18 20:48:40 -0500

    Finishing class #97-#98 with homework.

---
+++ notes/03-testing/01-Pruebas-sobre-hooks.md
@@ -0,0 +1,47 @@
+# #01. Pruebas sobre hooks
+
+Para realizar pruebas sobre hooks, hay que hacer uso de la función de Testing Library llamada `renderHook`, que se especializa en hacer funcionar hooks. Fue creada debido a que la función `render` no permite usar hooks, porque los hooks deben usarse únicamente dentro de un componente de React.
+
+Como parámetro, esta función solicita el hook, que a veces se deberá usar un callback para llamarlo. Por ejemplo:
+
+```tsx
+const { result } = renderHook(() => useCounter());
+const { result } = renderHook(useCounter);
+```
+
+# Cambiar estado de los hooks
+
+---
+
+La función `renderHook` devuelve un objeto que, entre otras cosas, tiene el “resultado” (`result`) y dentro de él está el resultado actual (`current`), ahí está lo que devuelve el hook y, por tanto, se puede llamar a los métodos del hook que sirven para actualizar el estado.
+
+Sin embargo, hay un problema, se trata de que el estado al actualizarse directamente en la prueba con `result.current`, el estado no se actualizará. De hecho, la prueba lanza el siguiente mensaje:
+
+```
+An update to TestComponent inside a test was not wrapped in act(...).
+
+When testing, code that causes React state updates should be wrapped into act(...):
+
+act(() => {
+  /* fire events that update state */
+});
+/* assert on the output */
+
+This ensures that you're testing the behavior the user would see in the browser. Learn more at https://react.dev/link/wrap-tests-with-act
+```
+
+En pocas palabras, en mensaje dice lo siguiente: \*\*Cualquier actualización de estado debe estar envuelto dentro de un callback en `act`. Por tanto, para actualizar el estado, más o menos debe ser de la siguiente manera:
+
+```tsx
+act(() => {
+  result.current.handleAdd();
+});
+```
+
+## El problema de actualización de estado
+
+Es posible actualizar el mismo estado con métodos repetidos, sin embargo, hay que tener en cuenta cómo se actualiza el estado en el custom hook:
+
+1. Si el estado se actualiza de manera directa con el estado deseado, siempre se actualizará con el estado antes del `act`, aunque se repita el mismo método. Esto se debe a que cada vez que se llama al método utiliza el estado actual, iniciando un proceso de actualización del hook que, mientras ocurre, los otros métodos se quedan actualizando el mismo estado que estaba antes del `act`. Un ejemplo de este tipo de actualización es: `setCounter(counter - 1)`.
+
+2. Si el estado se actualiza con un callback que utiliza el estado anterior, ahí sí se logra el efecto esperado. Lo que sucede es confuso, pero pasa porque mientras ocurren las actualizaciones del estado, el callback o su estado anterior que se manda cómo argumento permite el comportamiento deseado. En palabras más simples, tanto el callback cómo el estado anterior pueden lograr que se logre la actualización deseada. Un ejemplo de este tipo de actualización es: `setCounter((prevState) => prevState - 1)`.
---
+++ src/counter/hooks/useCounter.test.tsx
@@ -0,0 +1,91 @@
+import { describe, expect, test } from "vitest";
+import { act, renderHook } from "@testing-library/react";
+import { useCounter } from "./useCounter";
+
+describe("useCounter", () => {
+  // Default hook initial value for tests.
+  // This will be only used for keep value in case of changing the
+  // default value on useCounter.
+  // Note that this will not be used for call hooks.
+  const defaultInitialValue = 10;
+
+  // let { result } = renderHook(() => useCounter());
+  // beforeEach(() => {
+  //   result = renderHook(() => useCounter()).result;
+  // });
+
+  test("Should initialize with default value (10).", () => {
+    const { result } = renderHook(useCounter);
+
+    expect(result.current.counter).toBe(10);
+  });
+  test("Should initialize with value '20'.", () => {
+    const initialValue = 20;
+
+    const { result } = renderHook(() => useCounter(initialValue));
+
+    expect(result.current.counter).toBe(initialValue);
+  });
+
+  // increment handleAdd
+  // decrement handleSubtract
+  // reset handleReset
+  test("Should increment when handleAdd is called.", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.handleAdd();
+    });
+
+    expect(result.current.counter).toBe(defaultInitialValue + 1);
+  });
+  test("Should reduce when handleSubtract is called.", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(result.current.handleSubtract);
+
+    expect(result.current.counter).toBe(defaultInitialValue - 1);
+  });
+  describe("Should reset to default value when handleReset is called...", () => {
+    test("when handleAdd was called two times.", () => {
+      const { result } = renderHook(() => useCounter());
+
+      act(result.current.handleAdd);
+      act(result.current.handleAdd);
+      act(result.current.handleReset);
+
+      expect(result.current.counter).toBe(defaultInitialValue);
+    });
+    test("when handleSubtract was called two times.", () => {
+      const { result } = renderHook(() => useCounter());
+
+      act(result.current.handleSubtract);
+      act(result.current.handleSubtract);
+      act(result.current.handleReset);
+
+      expect(result.current.counter).toBe(defaultInitialValue);
+    });
+  });
+  describe("Should reset to custom value (40) when handleReset is called.", () => {
+    const initialValue = 40;
+
+    test("when handleAdd was called two times.", () => {
+      const { result } = renderHook(() => useCounter(initialValue));
+
+      act(result.current.handleAdd);
+      act(result.current.handleAdd);
+      act(result.current.handleReset);
+
+      expect(result.current.counter).toBe(initialValue);
+    });
+    test("when handleSubtract was called two times.", () => {
+      const { result } = renderHook(() => useCounter(initialValue));
+
+      act(result.current.handleSubtract);
+      act(result.current.handleSubtract);
+      act(result.current.handleReset);
+
+      expect(result.current.counter).toBe(initialValue);
+    });
+  });
+});
